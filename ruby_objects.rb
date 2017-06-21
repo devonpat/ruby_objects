6b966bf4d723cfbce1d636d39fadcd9d4ac0c841
Devon Patey <devon.patey@gmail.com>
2017-06-21 17:25:33 -0600

    finished

--- ruby_objects.rb
+++ ruby_objects.rb
@@ -25,3 +25,41 @@ puts Calculator.divide 10,2
 
 # part 2 elevator
 
+#Instance Methods:
+
+#The following methods will change the floor instance variable:
+#go_up - tell the elevator to go up a floor
+#go_down - tell the elevator to go down a floor
+#floor - set this as an accessible attribute to be accessed outside of the class definition
+#cheery_greeting - puts to the terminal the current floor with an interesting greeting
+#Use the cheery_greeting method to display your greeting every time you change floors
+
+class Elevator
+	attr_accessor :floor
+	attr_accessor :rise
+	attr_accessor :fall
+	
+	def initialize (floor)
+ 		@floor = floor
+ 		@rise = rise
+ 		@fall = fall
+	end	
+
+	def goUp
+		@floor += 1
+	end
+
+	def goDown
+		@floor -= 1
+	end
+end
+
+deathtrap = Elevator.new 1
+deathtrap.goUp
+puts "You have arrived at floor number #{deathtrap.floor}"
+deathtrap.goUp
+puts "You have arrived at floor number #{deathtrap.floor}"
+deathtrap.goUp
+puts "You have arrived at floor number #{deathtrap.floor}"
+deathtrap.goDown
+puts "You have fallen to floor number #{deathtrap.floor}"
