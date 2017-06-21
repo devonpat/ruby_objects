152bd14e81a85c15ab9adf8ed5cd95bfdf3c6bba
Devon Patey <devon.patey@gmail.com>
2017-06-21 17:01:40 -0600

    part 1

---
+++ ruby_objects.rb
@@ -0,0 +1,27 @@
+# part 1 calculator
+
+class Calculator
+	def self.add(a,b)
+		a + b
+	end
+
+	def self.subtract(c,d)
+		c - d
+	end
+
+	def self.multiply(e,f)
+		e * f
+	end
+
+	def self.divide(g,h)
+		g / h
+	end
+end
+
+puts Calculator.add 10,15
+puts Calculator.subtract 15,10
+puts Calculator.multiply 5,2
+puts Calculator.divide 10,2
+
+# part 2 elevator
+
